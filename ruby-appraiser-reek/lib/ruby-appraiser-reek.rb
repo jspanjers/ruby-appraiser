2af1d0d00f5df5279795b0bc840be24571774219
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-04-04 21:57:00 +0000

    fixes #1; provide reek adapter with graceful return when attempting to run against an empty set

--- ruby-appraiser-reek/lib/ruby-appraiser-reek.rb
+++ ruby-appraiser-reek/lib/ruby-appraiser-reek.rb
@@ -9,6 +9,7 @@ module RubyAppraiserReek
   class ReekAdapter < RubyAppraiser::Adapter
 
     def appraise
+      return false if relevant_files.empty?
       file_args = Shellwords::join(relevant_files)
       file_args = '**/*.rb' if file_args.length > 250_000
 
@@ -18,6 +19,7 @@ module RubyAppraiserReek
       puts reek_command if @options[:verbose]
 
       reek_yaml = IO.popen(reek_command) { |io| io.read }
+      return false if reek_yaml.chomp.empty?
       reek_output = YAML.load(reek_yaml)
       reek_output.each do |smell|
         Array(smell.lines).each do |line|
